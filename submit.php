86873f95f64425c73b92cd229f18635ed75edf15
Harsha Gotur <hgotur@umich.edu>
2013-02-27 19:46:59 -0500

    Changed parsing function

---
+++ submit.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+<head>
+		<title>Submit Page</title>
+		<!-- Bootstrap -->
+		<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+	  	<link rel="stylesheet" type="text/css" href="style.css" />
+	  	<link rel="stylesheet" type="text/css" href="pageslide/jquery.pageslide.css" />
+</head>
+<body>
+
+</body>
