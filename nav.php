aec24134ca1cb97b5fa57b768c728362a4d23cbc
Harsha Gotur <hgotur@umich.edu>
2013-04-07 19:17:34 -0400

    update css for nav.php

--- nav.php
+++ nav.php
@@ -6,43 +6,13 @@ if(!isset($_COOKIE['uniqname']))
 ?>
 
 <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
-<style type="text/css">
-
-.brand
-{
-	margin-top: 10px; 
-	text-align: center;
-	width: 66%;
-}
-#uniqtext
-{
-	font-size: 50px;
-	vertical-align: middle;
-}
-.logout
-{
-	margin-right: 10px;
-	margin-left: 10px;
-	height: 50px;
-	width: 50px;
-}
-
-.plus1
-{
-	margin-left: 10px;
-	margin-right: 10px;
-	height: 53px;
-	width: 53px;
-}
-
-</style>
-
+<link href="nav_style.css" rel="stylesheet" media="screen">
 
 <html>
-	<div class="navbar">
+	<div class="navbar navbar-inverse">
 	  <div class="navbar-inner">
 	    <a class="brand" href="#"><span id="uniqtext"><?php echo $_COOKIE["uniqname"]?><span></a>
-	    <ul class="nav">
+	    <ul class="nav pull-right">
 	      <li class="icon"><a href="#"><img class="logout" src="logout.png"></a></li>
 	      <li class="icon"><a href="#"><img class="plus1" src="plus1.png"></a></li>
 	    </ul>
