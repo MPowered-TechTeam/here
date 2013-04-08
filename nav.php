c19d7afaae3277eec734ba22406c3b75c44eb452
Jeff Sallans <jeffsallans@gmail.com>
2013-04-07 23:27:51 -0400

    linked nav buttons

--- nav.php
+++ nav.php
@@ -5,6 +5,7 @@ if(!isset($_COOKIE['uniqname']))
 	header( 'Location: index.php');
 ?>
 <script src="http://code.jquery.com/jquery-latest.js"></script>
+<script src="index_cookie.js"></script>
 <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
 <link href="nav_style.css" rel="stylesheet" media="screen">
 
@@ -13,19 +14,21 @@ if(!isset($_COOKIE['uniqname']))
     <a class="brand" href="#"><span id="uniqtext"><?php echo $_COOKIE["uniqname"]?><span></a>
     <ul class="nav pull-right">
       <li class="icon"><a href="#"><img class="logout" src="images/exit.png"></a></li>
-      <li class="icon"><a href="#"><img class="plus1" src="images/plus.png"></a></li>
+      <li class="icon"><a href="create_event.php"><img class="plus1" src="images/plus.png"></a></li>
     </ul>
   </div>
 </div>
 
 <script type="text/javascript">
-$(".logout").click(
-	function()
-	{
-		setCookie("uniqname", "",  -1);
-		window.location = "index.php";
-	}
-)
+$(function() {
+  $(".logout").click(
+  	function()
+  	{
+  		setCookie("uniqname", "",  -1);
+  		window.location = "index.php";
+  	}
+  );
+});
 </script>
 
 
