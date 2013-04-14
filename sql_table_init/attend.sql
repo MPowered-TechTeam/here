6a1ef2958707015d39bdc51610b465f76450cb50
Jeff Sallans <jeffsallans@gmail.com>
2013-04-14 18:30:57 -0400

    change .sql files

--- sql_table_init/attend.sql
+++ sql_table_init/attend.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 28, 2013 at 12:39 AM
+-- Generation Time: Apr 14, 2013 at 10:29 PM
 -- Server version: 5.5.16
 -- PHP Version: 5.3.8
 
@@ -29,10 +29,33 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `attend` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `event_id` int(11) NOT NULL,
-  `name` varchar(30) NOT NULL,
   `uniqname` varchar(30) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  PRIMARY KEY (`id`),
+  KEY `event_id` (`event_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+
+--
+-- Dumping data for table `attend`
+--
+
+INSERT INTO `attend` (`id`, `event_id`, `uniqname`) VALUES
+(1, 28, 'jsallans'),
+(2, 31, 'jasleung'),
+(3, 18, 'jsallans'),
+(4, 33, 'jsallans'),
+(5, 31, 'jsallans'),
+(7, 30, 'jsallans'),
+(8, 29, 'jsallans');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `attend`
+--
+ALTER TABLE `attend`
+  ADD CONSTRAINT `attend_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- sql_table_init/event.sql
+++ sql_table_init/event.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 07, 2013 at 11:05 PM
+-- Generation Time: Apr 14, 2013 at 10:29 PM
 -- Server version: 5.5.16
 -- PHP Version: 5.3.8
 
@@ -28,20 +28,27 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `event` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `active` tinyint(4) NOT NULL DEFAULT '1',
   `name` varchar(50) NOT NULL,
   `creator` varchar(30) NOT NULL,
   `long` double NOT NULL,
   `lat` double NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=26 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=34 ;
 
 --
 -- Dumping data for table `event`
 --
 
-INSERT INTO `event` (`id`, `name`, `creator`, `long`, `lat`) VALUES
-(18, 'R2220', 'mlripper', -83.73820359999999, 42.27253779999999),
-(25, 'MPowered Talent Meeting', 'mlripper', -83.7362886, 42.27522860000001);
+INSERT INTO `event` (`id`, `active`, `name`, `creator`, `long`, `lat`) VALUES
+(18, 1, 'R2220', 'mlripper', -83.73820359999999, 42.27253779999999),
+(25, 1, 'MPowered Talent Meeting', 'mlripper', -83.7362886, 42.27522860000001),
+(28, 1, '3rd floor dude', 'jsallans', -83.71582389999999, 42.2909548),
+(29, 1, 'dude 3rd floor with jill', 'jsallans', -83.7155959, 42.2912813),
+(30, 1, 'CFE ugli space', 'jsallans', -83.736989, 42.2756761),
+(31, 1, 'home', 'jsallans', -83.72647549999999, 42.2946434),
+(32, 1, 'tech team meeting', 'jsallans', -83.716155, 42.2927885),
+(33, 1, 'Ugli middle tables', 'jsallans', -83.7367922, 42.2755647);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
