d055b951af9c1eaa3c5f629083af9fde47ae1bcc
Ryan Cesiel <cesiel1993@gmail.com>
2013-04-16 14:09:05 -0400

    Send attendee names

---
+++ ajax/send_attendeenames.php
@@ -0,0 +1,27 @@
+<?php
+$uniqname = $_COOKIE['uniqname'];
+$eventname = $_REQUEST['event'];
+$messagebody = $_REQUEST['message'];
+
+// multiple recipients
+$to  = '$uniqname . '@umich.edu'';
+
+// subject
+$subject = 'Attendee Names for' . $eventname;
+
+// message
+$message = $messagebody;
+
+// To send HTML mail, the Content-type header must be set
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+// Additional headers
+$headers .= 'To:' . $to . "\r\n";
+$headers .= 'From: MPowered Tech Team <mpowered-techteam@umich.edu>' . "\r\n";
+
+// Mail it
+mail($to, $subject, $message, $headers);
+
+echo "success message";
+?>
